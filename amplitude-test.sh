97c9c01f64e27421a3f4d09aaaed5f2c92526dda
Allan Carroll <allanca@gmail.com>
2015-04-14 16:46:16 -0700

    Add automated test for building against release

---
+++ amplitude-test.sh
@@ -0,0 +1,32 @@
+#!/usr/local/bin/bash
+
+##
+# Test that the amplitude jars compile against a real project.
+# Tests both a local jar and the gradle SNAPSHOT release. This should be run
+# after uploading a new release to sonatype and staging the build on github
+# and before publishing the build.
+##
+
+set -e
+
+if [ $# -eq 0 ]; then
+    echo "./amplitude-test [amplitude version]"
+    exit 1
+fi
+
+VERSION=$1
+BASE_VERSION=`echo $1 | sed -e "s/-SNAPSHOT//"`
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+JAR="amplitude-android-${BASE_VERSION}-with-dependencies.jar"
+
+cd ${DIR}
+
+wget "https://github.com/amplitude/Amplitude-Android/blob/v${BASE_VERSION}-snapshot/${JAR}?raw=true" -O ${DIR}/app/libs/${JAR}
+
+echo "ext.amplitudeVersion = '${BASE_VERSION}'" > ${DIR}/config.gradle
+echo "ext.amplitudeUseLocal = true" >> ${DIR}/config.gradle
+./gradlew assembleRelease
+
+echo "ext.amplitudeVersion = '${VERSION}'" > ${DIR}/config.gradle
+echo "ext.amplitudeUseLocal = false" >> ${DIR}/config.gradle
+./gradlew assembleRelease
